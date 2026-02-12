0f08304a43490ff81644aef72f64332ab10c030a
Fernando Nina <Fernandonina611@gmail.com>
2026-02-12 17:28:37 -0400

    feat: :sparkles: agregando beforeInsert para generar Slugs

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,54 +1,65 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { v7 as uuidv7 } from 'uuid';
 @Entity()
 export class Product {
-    @PrimaryGeneratedColumn('uuid')
-    id: string = uuidv7(); //Usamos Version 7 de uuid
+  @PrimaryGeneratedColumn('uuid')
+  id: string = uuidv7(); //Usamos Version 7 de uuid
 
-    @Column({
-        type: 'varchar',
-        unique: true,
-    })
-    title: string;
+  @Column({
+    type: 'varchar',
+    unique: true,
+  })
+  title: string;
 
+  @Column({
+    type: 'float',
+    default: 0,
+  })
+  price: number;
 
-    @Column({
-        type: 'float',   
-        default: 0
-    })
-    price: number;
-    
-    @Column({
-        type: 'text',
-        nullable: true
-    })
-    description: string;
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description: string;
 
-    @Column({
-        type: 'varchar',
-        unique: true,
-    })
-    slug: string;
+  @Column({
+    type: 'varchar',
+    unique: true,
+  })
+  slug: string;
 
-    @Column({
-        type: 'int',
-        default: 0
-    })
-    stock: number;
+  @Column({
+    type: 'int',
+    default: 0,
+  })
+  stock: number;
 
-    @Column({
-        type: 'text',
-        array: true,
-        default: []
-    })
-    sizes: string[];
+  @Column({
+    type: 'text',
+    array: true,
+    default: [],
+  })
+  sizes: string[];
 
-    @Column({
-        type: 'varchar',
-        default: 'unisex'
-    })
-    gender: string;
+  @Column({
+    type: 'varchar',
+    default: 'unisex',
+  })
+  gender: string;
 
-    //tags
-    //images
+  //tags
+  //images
+
+  @BeforeInsert()
+  checkSlugInsert() {
+    if (!this.slug) {
+      this.slug = this.title;
+    }
+    this.slug = this.generateSlug(this.slug);
+  }
+
+  private generateSlug(string: string) {
+    return string.toLocaleLowerCase().replaceAll(' ', '-').replace("'", '');
+  }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -8,6 +8,8 @@ import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+
+  private readonly logger = new Logger(ProductsService.name);
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
@@ -16,15 +18,11 @@ export class ProductsService {
   async create(createProductDto: CreateProductDto) {
 
     try {
-      /* Generar slug */
-      const slug = createProductDto.title.toLowerCase().replace(/ /g, '-');
-      createProductDto.slug = slug;
       const producto = this.productRepository.create(createProductDto);
       await this.productRepository.save(producto);
       return producto;
     } catch (error) {
-      console.error(error);
-      throw new InternalServerErrorException('Ayuda..!!');
+      this.handleDBException(error);
     }
   }
 
@@ -42,5 +40,13 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+  private handleDBException(error: any) {
+    if (error.code === '23505')
+      throw new BadRequestException(error.detail);
+
+    this.logger.error(error);
+    throw new InternalServerErrorException('Erro inesperado, revisa los logs del servidor..!!');
   }
 }
