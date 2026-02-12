5066a73f77616905ebb83fad091ae40be83bfd3f
Fernando Nina <Fernandonina611@gmail.com>
2026-02-12 16:00:48 -0400

    chore: ajustes entidad productos

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,25 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { v7 as uuidv7 } from 'uuid';
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn('uuid')
+    id: string = uuidv7(); //Usamos Version 7 de uuid
+
+    @Column('varchar',{
+        unique: true,
+    })
+    title: string;
+
+
+    /* @Column('decimal', {
+        precision: 10,
+        scale: 2,
+    })
+    price: number;
+
+    @Column('text')
+    description: string;
+
+    @Column('varchar')
+    image: string; */
+}
