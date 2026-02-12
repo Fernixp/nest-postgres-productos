a0842f24a83c777b994f94ba1dff1598704555da
Fernando Nina <Fernandonina611@gmail.com>
2026-02-12 17:42:23 -0400

    feat: :sparkles: agregando funciones findall, findone y delete

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,14 +1,18 @@
-import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 
-
 @Injectable()
 export class ProductsService {
-
   private readonly logger = new Logger(ProductsService.name);
   constructor(
     @InjectRepository(Product)
@@ -16,7 +20,6 @@ export class ProductsService {
   ) {}
 
   async create(createProductDto: CreateProductDto) {
-
     try {
       const producto = this.productRepository.create(createProductDto);
       await this.productRepository.save(producto);
@@ -27,26 +30,32 @@ export class ProductsService {
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.productRepository.findAndCount();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    const product = await this.productRepository.findOneBy({ id });
+    if (!product)
+      throw new NotFoundException(`Producto no encontrado con id ${id}`);
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
+  update(id: string, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const producto = await this.findOne(id);
+    await this.productRepository.remove(producto);
+    return producto;
   }
 
   private handleDBException(error: any) {
-    if (error.code === '23505')
-      throw new BadRequestException(error.detail);
+    if (error.code === '23505') throw new BadRequestException(error.detail);
 
     this.logger.error(error);
-    throw new InternalServerErrorException('Erro inesperado, revisa los logs del servidor..!!');
+    throw new InternalServerErrorException(
+      'Erro inesperado, revisa los logs del servidor..!!',
+    );
   }
 }
