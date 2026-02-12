31bcdf0f73708eb392b768ce6a50dd3a62f40529
Fernando Nina <Fernandonina611@gmail.com>
2026-02-12 16:32:34 -0400

    feat: :sparkles: agregando validadores en entidad Productos

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,38 @@
-export class CreateProductDto {}
+import { IsArray, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString } from "class-validator";
+export enum Gender {
+  MEN = 'men',
+  WOMEN = 'women',
+  UNISEX = 'unisex',
+}
+
+export class CreateProductDto {
+    @IsString()
+    title: string;
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?: number;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsString()
+    @IsOptional()
+    slug?: string;
+
+    @IsNumber()
+    @IsOptional()
+    stock?: number;
+
+    @IsArray()
+    @IsString({ each: true })
+    @IsNotEmpty({each: true})
+    sizes: string[];
+
+    @IsString()
+    @IsEnum(Gender)
+    @IsNotEmpty()
+    gender: Gender;
+}
