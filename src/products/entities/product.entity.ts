5b5e90a24b4072e5190591d6b228ff8370e133ca
Fernando Nina <fernandonina611@gmail.com>
2026-02-14 21:38:33 -0400

    feat: :sparkles: cambiando nombres de tablas por defecto en entidades

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,7 +1,7 @@
 import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { v7 as uuidv7 } from 'uuid';
 import { ProductImage } from './product-image.entity';
-@Entity()
+@Entity({ name: 'products' })
 export class Product {
   @PrimaryGeneratedColumn('uuid')
   id: string = uuidv7(); //Usamos Version 7 de uuid
