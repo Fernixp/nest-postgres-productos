fee7c0d8aef71460774caff2e99b3d4ef02f935c
Fernando Nina <fernandonina611@gmail.com>
2026-02-15 17:22:26 -0400

    feat: :sparkles: agregando imagenes y habilitando rutas estaticas con index.html por defecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,13 @@
+/* Express */
+import { join } from 'path';
+
+/* Nest */
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+/* Propios del proyecto */
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
@@ -8,16 +15,22 @@ import { FilesModule } from './files/files.module';
 
 @Module({
   imports: [ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      database: process.env.POSTGRES_DB,
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    database: process.env.POSTGRES_DB,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    autoLoadEntities: true,
+    synchronize: true,
+  }),
+
+  /* Habilitando rutas estaticas para archivos */
+  ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'public'),
+  }),
+
     ProductsModule,
     CommonModule,
     SeedModule,
@@ -26,4 +39,4 @@ import { FilesModule } from './files/files.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
