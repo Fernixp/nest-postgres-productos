d2e9bef85520e3e3df8d34d4edbc51abb36f99a0
Fernando Nina <Fernandonina611@gmail.com>
2026-02-18 16:21:22 -0400

    feat: :sparkles: busqueda por titulo o slutg

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -11,6 +11,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { isUUID } from 'class-validator';
 
 @Injectable()
 export class ProductsService {
@@ -40,10 +41,25 @@ export class ProductsService {
     });
   }
 
-  async findOne(id: string) {
-    const product = await this.productRepository.findOneBy({ id });
+  async findOne(term: string) {
+    let product: Product | null;
+
+    if (isUUID(term)) {
+      product = await this.productRepository.findOneBy({ id: term });
+    } else {
+      const queryBuilder = this.productRepository.createQueryBuilder();
+
+      /* Agregar lowercase para comparar */
+      product = await queryBuilder
+        .where('LOWER(title) = :title or slug = :slug', {
+          slug: term,
+          title: term.toLowerCase(),
+        })
+        .getOne();
+    }
+    
     if (!product)
-      throw new NotFoundException(`Producto no encontrado con id ${id}`);
+      throw new NotFoundException(`Producto no encontrado con term ${term}`);
     return product;
   }
 
