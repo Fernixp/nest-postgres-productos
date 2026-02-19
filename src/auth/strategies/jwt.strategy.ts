7e2a226a3455192b695d742b176ebdd07f6e746a
Fernando Nina <Fernandonina611@gmail.com>
2026-02-19 13:27:02 -0400

    feat: :sparkles: creando jwt strategy para validar token y usuario activo

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,31 +1,34 @@
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 import { User } from '../entities/user.entity';
 import { JwtPayload } from '../interfaces/jwt-payload.interface';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-
     constructor(
         @InjectRepository(User)
         private readonly userRepository: Repository<User>,
-        private readonly configService: ConfigService
+        configService: ConfigService
     ) {
         super({
-            secretOrKey: configService.get('JWT_SECRET'),
+            secretOrKey: configService.get('JWT_SECRET')!,
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration: false,
         });
     }
 
     async validate(payload: JwtPayload): Promise<User> {
 
         const { email } = payload;
+        const user = await this.userRepository.findOneBy({ email });
 
-        const user = await 
-        
-        return {} as User;
+        if (!user) throw new UnauthorizedException('Token no válido');
+
+        if (!user.isActive) throw new UnauthorizedException('Usuario inactivo, contacte al administrador');
+
+        return user;
     }
 }
