9db37674172efc45a70ed00044f91cf611bbd5cc
Fernando Nina <Fernandonina611@gmail.com>
2026-02-19 17:05:33 -0400

    feat: :sparkles: agregando el auth en otros modulos

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,8 +3,11 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ValidRoles } from 'src/auth/interfaces/valid-roles';
 
 @Controller('v1/products')
+@Auth()
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) { }
 
