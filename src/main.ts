2eb2620af7cf6ada28645724e3d7caeb02287cd8
Fernando Nina <Fernandonina611@gmail.com>
2026-02-20 11:45:52 -0400

    feat: :sparkles: instalando swagger para documentacion de la api

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function main() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,14 @@ async function main() {
       forbidNonWhitelisted: true,
     }),
   );
+  /* Documentacion - Swagger */
+  const config = new DocumentBuilder()
+    .setTitle('API Productos')
+    .setDescription('API para gestionar productos')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
   await app.listen(port);
 
   logger.log(`Aplicacion ejecutandose en el puerto: ${port} `)
